dd0431a1aa5a411f5f4961dae9ad0858dcff36a3
rustem bayetov <rustem.bayetov@gmail.com>
2019-11-20 21:45:10 -0600

    config, deploy configs

---
+++ makefile
@@ -0,0 +1,36 @@
+img=islamahmad/eaproj-elastic-searchms:1.0.7
+
+# ===== Maven =====
+maven-rebuild:
+	mvn clean && mvn install
+
+# ===== Docker =====
+docker-build: maven-rebuild
+	docker build -t ${img} .
+
+docker-run:
+	docker run -p 8080:8092 ${img}
+
+docker-login:
+	docker login
+
+docker-push: docker-login docker-build
+	docker push ${img}
+k8-install:
+	kubectl apply -f k8s-deploy.yaml
+
+k8-delete:
+	kubectl delete -f k8s-deploy.yaml
+
+k8-restart: k8-delete k8-install
+
+
+
+build:
+	 mvn clean && mvn install && docker build -t ${img} . && docker push $(img)
+
+config:
+	kubectl apply -f k8s-config.yaml
+
+deploy:
+	kubectl apply -f k8s-deploy.yaml
--- src/main/java/ea/sof/ms_elastic_search/controller/ElasticSearchController.java
+++ src/main/java/ea/sof/ms_elastic_search/controller/ElasticSearchController.java
@@ -1,23 +1,20 @@
 package ea.sof.ms_elastic_search.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import ea.sof.ms_elastic_search.repository.RedisQuestionRepositoryImpl;
 import ea.sof.ms_elastic_search.service.ElasticSearchService;
 import ea.sof.ms_elastic_search.service.RedisQuestionService;
 import ea.sof.shared.queue_models.QuestionQueueModel;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -26,36 +23,54 @@ import java.util.Map;
 @CrossOrigin
 @RequestMapping("/elastic-search")
 public class ElasticSearchController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RedisQuestionRepositoryImpl.class);
 
-    @Value("${maxhit}")
-    public int maxhit;
+	@Value("${maxhit}")
+	public int maxhit;
 
-    @Value("${redis.timeout}")
-    public long timeout;
+	@Value("${redis.timeout}")
+	public long timeout;
 
-    @Autowired
-    ElasticSearchService elasticSearchService;
+	@Autowired
+	ElasticSearchService elasticSearchService;
 
-    @Autowired
-    RedisQuestionService redisQuestionService;
+	@Autowired
+	RedisQuestionService redisQuestionService;
 
-    @GetMapping("/search/{phrase}")
-    public List<QuestionQueueModel> searchByName(@PathVariable(name="phrase") final String phrase) {
+    @Value("${app.version}")
+    private String appVersion;
 
-        List<QuestionQueueModel> questions = null;
-
-        //check if data exists in redis
-        Map<String, QuestionQueueModel> redisQuestions = redisQuestionService.findAll(phrase);
-        if(redisQuestions != null && redisQuestions.size() > 0) {
-            //get from redis
-            questions = new ArrayList<QuestionQueueModel>(redisQuestions.values());
-            return  questions;
+    @GetMapping("/health")
+    public ResponseEntity<?> index() {
+        String host = "Unknown host";
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
 
-        //else, get from ElasticSearch engine and update Redis
-        questions = elasticSearchService.search(phrase);
-        redisQuestionService.saveAll(phrase, questions, timeout);
-
-        return questions;
+        return new ResponseEntity<>("Elastic search service (" + appVersion + "). Host: " + host, HttpStatus.OK);
     }
+
+	@GetMapping("/search/{phrase}")
+	public List<QuestionQueueModel> searchByName(@PathVariable(name = "phrase") final String phrase) {
+		LOGGER.info("New search: " + phrase);
+
+		List<QuestionQueueModel> questions = null;
+
+		//check if data exists in redis
+		Map<String, QuestionQueueModel> redisQuestions = redisQuestionService.findAll(phrase);
+		if (redisQuestions != null && redisQuestions.size() > 0) {
+			questions = new ArrayList<QuestionQueueModel>(redisQuestions.values());
+			LOGGER.info("Found " + questions.size() + " records from Redis");
+			return questions;
+		}
+
+		//else, get from ElasticSearch engine and update Redis
+		questions = elasticSearchService.search(phrase);
+		LOGGER.info("Found " + questions.size() + " records from elastic search engine");
+		redisQuestionService.saveAll(phrase, questions, timeout);
+
+		return questions;
+	}
 }
